f167a64deb209dee682485a9ab58ab53490771df
Vsevolod <vsevolod.lashin@gmail.com>
2023-11-01 11:08:05 +0400

    Initial Commit

---
+++ QuadraticEquation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuadraticEquation
+//
+//  Created by Vsevolod Lashin on 01.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
